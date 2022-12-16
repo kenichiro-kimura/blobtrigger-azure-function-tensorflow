fe0aa629600163139cd1b9f7b80e0ca68a0be561
k-kimura <k-kimura@alterbooth.com>
2022-12-16 15:32:52 +0900

    [modify] update syntax error

--- BlobTrigger/__init__.py
+++ BlobTrigger/__init__.py
@@ -111,6 +111,6 @@ def send_linenotify(probability,image_url):
     line_token = os.environ.get('LineToken')
     headers = {"Content-Type": "application/x-www-form-urlencoded","Authorization": "Bearer %s" % line_token};
 
-    message = "玄関の鍵が開いてるかもしれません！\n(確率 %s)\n%s\n" % probability,image_url
+    message = "玄関の鍵が開いてるかもしれません！\n(確率 %s)\n%s\n" % (probability,image_url)
     data = {"message": message.encode("utf-8")}
     requests.post(URL, headers=headers, data=data);
